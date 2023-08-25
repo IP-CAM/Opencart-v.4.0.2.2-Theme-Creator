9db3389a70a4d954e4b9f4170d56dbac50160317
Yaroslav Glodov <glodov@gmail.com>
2023-08-25 22:40:37 +0300

    Shorter documentation.

--- theme-creator.php
+++ theme-creator.php
@@ -2,13 +2,13 @@
 
 if ($argc < 3) {
     die("
-    Usage: php ./theme-creator.php [name] [output_dir|output_file] [-a author_name] [-u author_link] [-v version]
-            theme_name  - The name of the theme.
-            output_dir  - The path to the system/storage/marketplace directory or another one if you wish.
-            output_file - The path to the archived \${theme_name}.ocmod.zip file if ends up with .zip.
-            author_name - Name of the theme's author, default is \"Yaro\".
-            author_link - Link to the website of the theme's author, default is https://code.yaro.page/.
-            version     - The version, default is 1.0.
+    Usage: php ./theme-creator.php [name] [out_dir|out_file] [-a author] [-u url] [-v version]
+            name     - The name of the theme.
+            out_dir  - The path to the system/storage/marketplace directory or another one if you wish.
+            out_file - The path to the archived \${theme_name}.ocmod.zip file if ends up with .zip.
+            author   - Name of the theme's author, default is \"Yaro\".
+            url      - Link to the website of the theme's author, default is https://code.yaro.page/.
+            version  - The version, default is 1.0.
     ");
 }
 
